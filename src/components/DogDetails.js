c0819ebf6fc28f8c93ecff18b737e8718f174c13
Nogal1 <mcclendon.logan1234@gmail.com>
2024-09-15 14:38:55 -0700

    components implemented

---
+++ src/components/DogDetails.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useParams, Redirect } from "react-router-dom";
+
+function DogDetails({ dogs }) {
+    const { name } = useParams();
+    const dog = dogs.find(dog => dog.name.toLowerCase() === name.toLocaleLowerCase());
+
+    if (!dog) {
+        return <Redirect to="/dogs" />;
+    }
+
+    return (
+        <div>
+            <h1>{dog.name}</h1>
+            <img src={dog.src} alt={dog.name} width="300" />
+            <h3>Age: {dog.age}</h3>
+            <ul>
+            {dog.facts.map((fact, i) => (
+          <li key={i}>{fact}</li>
+        ))}
+            </ul>
+            <a href="/dogs">Back to all dogs</a>
+        </div>
+    );
+}
+
+export default DogDetails;
---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Nav({ dogs }) {
+    return (
+        <nav>
+      <NavLink exact to="/dogs">
+        Home
+      </NavLink>
+      {dogs.map(name => (
+        <NavLink key={name} to={`/dogs/${name.toLowerCase()}`}>
+          {name}
+        </NavLink>
+      ))}
+    </nav>
+  );
+}
+
+export default Nav;
